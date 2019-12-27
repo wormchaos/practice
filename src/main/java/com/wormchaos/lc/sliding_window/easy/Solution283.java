bf4f16bbcc329c57532607b5428667938c6b85de
wormchaos <wormchaos@126.com>
2019-12-28 01:07:51 +0800

    wormchaos:优化置0操作

--- src/main/java/com/wormchaos/lc/sliding_window/easy/Solution283.java
+++ src/main/java/com/wormchaos/lc/sliding_window/easy/Solution283.java
@@ -1,14 +1,29 @@
 package com.wormchaos.lc.sliding_window.easy;
 
 
-import com.wormchaos.lc.toolbean.TreeNode;
-
 /**
  * Created by wormchaos on 2019-12-28.
  * move 0
  */
 public class Solution283 {
+
     public void moveZeroes(int[] nums) {
+        int zeroPoint = 0;
+        int i = 0;
+        while(i < nums.length) {
+            if(nums[i] == 0) {
+                zeroPoint = i;
+            } else {
+                nums[zeroPoint] = nums[i];
+                zeroPoint = i;
+            }
+        }
+    }
+
+    /**
+     * 该方法需要加额外数组
+     */
+    public void moveZeroesV1(int[] nums) {
         int[] a = new int[nums.length];
         int count = 0;
         for(int i=0; i < nums.length; i++) {
