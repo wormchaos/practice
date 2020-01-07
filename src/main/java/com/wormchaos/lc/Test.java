f1a3b8d755ff3f9e0db563d7457e8fe322ded887
wormchaos <wormchaos@126.com>
2020-01-07 21:02:05 +0800

    wormchaos:解决dp问题

--- src/main/java/com/wormchaos/lc/Test.java
+++ src/main/java/com/wormchaos/lc/Test.java
@@ -150,7 +150,7 @@ public class Test {
 //        Object result = new Solution264().nthUglyNumber(10);
 //        Object result = new Solution322().coinChange(new int[]{186,419,83,408}, 6249);
 //        Object result = new Solution322().coinChange(new int[]{2,4}, 5);
-        Object result = new Solution375().getMoneyAmount(5);
+        Object result = new Solution375().getMoneyAmount(100);
         print(result);
 
 
--- src/main/java/com/wormchaos/lc/sliding_window/normal/Solution375.java
+++ src/main/java/com/wormchaos/lc/sliding_window/normal/Solution375.java
@@ -9,18 +9,27 @@ public class Solution375 {
      * @return
      */
     public int getMoneyAmount(int n) {
-        // 一个数的场景 min = 0
-        // 两个数的场景 n1,n2，min = n1
-        // 三个数的场景 n1,n2,n3 min = n2
-        // 四个数的场景 1 1 2 || 1 3
-        // 则为 s(n2-n3) + n1 = n1 + n3
-        // 五个数的场景 则为
-//        getMoney(1, n, 0);
-        //f[1,n] = f[1,k] + k + f[k+1, n]
-        return getMoney(1, n);
+        int[][] dp = new int[n + 1][n + 1];
+        // 不能根据左右来计算，因为左右可能会得到未计算的0长度值
+        // 设开始s,长度len
+        for (int len = 2; len <= n; len++) {
+            for (int s = 1; s <= n - len + 1; s++) {
+                int min = Integer.MAX_VALUE;
+                for (int k = (2 * s + len - 1) / 2; k < s + len - 1; k++) {
+                    int t = Math.max(dp[s][k - 1], dp[k + 1][s + len - 1]) + k;
+                    if (t < min) {
+                        min = t;
+                    }
+                }
+                dp[s][s + len - 1] = min;
+
+            }
+        }
+
+        return dp[1][n];
     }
 
-    private int getMoney(int l, int r) {
+    private int getMoney(int l, int r, int[][] dp) {
         if (r == l) {
             return 0;
         }
@@ -31,15 +40,19 @@ public class Solution375 {
             return l + 1;
         }
         if (r - l == 3) {
-            return l + l +2;
+            return l + l + 2;
+        }
+        if (0 != dp[r][l]) {
+            return dp[r][l];
         }
         int min = Integer.MAX_VALUE;
         for (int i = l + 1; i < r - 1; i++) {
-            int result = Math.max(getMoney(l, i - 1), getMoney(i + 1, r)) + i;
+            int result = Math.max(getMoney(l, i - 1, dp), getMoney(i + 1, r, dp)) + i;
             if (result < min) {
                 min = result;
             }
         }
+        dp[l][r] = min;
         return min;
     }
 }
