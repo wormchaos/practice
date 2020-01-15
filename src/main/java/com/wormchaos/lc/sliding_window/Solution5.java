71679921f486135f8a13cbb173bec744acb82665
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-01-15 18:48:35 +0800

    wormchaos:中心拓展

--- src/main/java/com/wormchaos/lc/sliding_window/Solution5.java
+++ src/main/java/com/wormchaos/lc/sliding_window/Solution5.java
@@ -7,25 +7,62 @@ package com.wormchaos.lc.sliding_window;
 public class Solution5 {
 
     /**
-     * dp
+     * 中心拓展
      *
      * @param s
      * @return
      */
     public String longestPalindrome(String s) {
+        if(s.length() == 0) {
+            return s;
+        }
+        int start = 0;
+        int end = 0;
+        //拓展中心
+        for (int i = 0; i < s.length(); i++) {
+            // i和i+1之间
+            int max1 = extend(s, i, i + 1) * 2;
+            // i+1做中心
+            int max2 = 2 * extend(s, i, i) - 1;
+            int maxT = Math.max(max1, max2);
+            if (maxT > end - start) {
+                start = i - (maxT - 1) / 2;
+                end = i + maxT / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int extend(String s, int l, int r) {
+        int count = 0;
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * dp
+     *
+     * @param s
+     * @return
+     */
+    public String longestPalindrome_DP(String s) {
         String maxString = "";
         boolean[][] dp = new boolean[s.length() + 1][s.length() + 1];
         for (int len = 1; len <= s.length(); len++) {
             boolean f = false;
             for (int left = 0; left <= s.length() - len; left++) {
                 int right = left + len - 1;
-                if(len == 1) {
+                if (len == 1) {
                     dp[left][right] = true;
-                    dp[left+1][left] = true;
+                    dp[left + 1][left] = true;
                 } else {
                     dp[left][right] = dp[left + 1][right - 1] && s.charAt(left) == s.charAt(right);
                 }
-                if(dp[left][right] && !f) {
+                if (dp[left][right] && !f) {
                     f = true;
                     maxString = s.substring(left, right + 1);
                 }
