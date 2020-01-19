ad0ff92cd13bbb48c39bd420c766333892b6296c
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-01-19 10:22:06 +0800

    wormchaos:dp优化平衡背包

--- src/main/java/com/wormchaos/lc/sliding_window/normal/Solution416.java
+++ src/main/java/com/wormchaos/lc/sliding_window/normal/Solution416.java
@@ -4,9 +4,46 @@ package com.wormchaos.lc.sliding_window.normal;
  * Created by wormchaos on 2020-1-19.
  */
 public class Solution416 {
-    boolean hasResult = false;
 
     public boolean canPartition(int[] nums) {
+        // 获取预期值
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if (nums.length <= 1 || sum % 2 == 1) {
+            return false;
+        }
+        // 找到数组值 等于总和一半即可
+        sum /= 2;
+        // define a,b - [0, a] value == sum
+        boolean[][] dp = new boolean[nums.length][sum + 1];
+        dp[0][0] = false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (i == 0) {
+                    dp[i][j] = j == nums[i];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || (j > nums[i] && dp[i - 1][j - nums[i]]);
+                }
+            }
+            if(dp[i][sum]) {
+                return true;
+            }
+        }
+        return dp[nums.length - 1][sum];
+
+    }
+
+    boolean hasResult = false;
+
+    /**
+     * 回溯(超时)
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canPartitionV1(int[] nums) {
         // 获取预期值
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
