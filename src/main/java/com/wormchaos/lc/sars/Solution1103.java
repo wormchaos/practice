aeddf29c721b27b2a160aa7db7cbe921c0cd4418
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-03-05 09:52:09 +0800

    wormchaos:分糖果

---
+++ src/main/java/com/wormchaos/lc/sars/Solution1103.java
@@ -0,0 +1,23 @@
+package com.wormchaos.lc.sars;
+
+/**
+ * Created by wormchaos on 2020-3-5.
+ */
+public class Solution1103 {
+
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] a = new int[num_people];
+        int p = 0;
+        int num = 1;
+        while (candies > 0) {
+            if(candies < num) {
+                num = candies;
+            }
+            a[p] += num;
+            candies -= num;
+            num++;
+            p = (p+1) % num_people;
+        }
+        return a;
+    }
+}
