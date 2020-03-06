9ffbbc3035ad0eadfe257f82f534b859ce7a6f71
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-03-06 11:46:25 +0800

    wormchaos:双指针

--- src/main/java/com/wormchaos/lc/sliding_window/easy/Solution141.java
+++ src/main/java/com/wormchaos/lc/sliding_window/easy/Solution141.java
@@ -4,28 +4,77 @@ import com.wormchaos.lc.toolbean.ListNode;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by wormchaos on 2020-3-6.
  */
 public class Solution141 {
+
+    /**
+     * 双指针
+     * @param head
+     * @return
+     */
     public boolean hasCycle(ListNode head) {
-        if(null == head) {
+        if(null == head || null == head.next) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if(fast == null || fast.next == null) {
+               return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+
+    }
+
+    /**
+     * hash
+     * @param head
+     * @return
+     */
+    public boolean hasCycleVHash(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+
+    }
+
+    /**
+     * fool
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycleV1(ListNode head) {
+        if (null == head) {
             return false;
         }
         Map<Integer, List<ListNode>> map = new HashMap<>();
         while (head.next != null) {
-            if(map.containsKey(head.next.val)) {
+            if (map.containsKey(head.next.val)) {
                 List<ListNode> temp = map.get(head.next.val);
                 for (ListNode listNode : temp) {
-                    if(listNode == head.next) {
+                    if (listNode == head.next) {
                         return true;
                     }
                 }
             }
-            if(map.containsKey(head.val)) {
+            if (map.containsKey(head.val)) {
                 map.get(head.val).add(head);
             } else {
                 List<ListNode> l = new ArrayList<ListNode>();
