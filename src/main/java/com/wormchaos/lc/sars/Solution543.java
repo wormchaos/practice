c06f870348c2bcfea30d85bd04059d01eddb4f73
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-03-10 17:11:08 +0800

    wormchaos:最长路径

---
+++ src/main/java/com/wormchaos/lc/sars/Solution543.java
@@ -0,0 +1,31 @@
+package com.wormchaos.lc.sars;
+
+import com.wormchaos.lc.toolbean.TreeNode;
+
+/**
+ * Created by wormchaos on 2020-3-10.
+ */
+public class Solution543 {
+
+    private int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        calc(root);
+        return max;
+    }
+
+    private int calc(TreeNode node) {
+        if (null == node) {
+            return 0;
+        }
+        if (null == node.left && null == node.right) {
+            return 1;
+        }
+        int l = calc(node.left);
+        int r = calc(node.right);
+        if (max < l + r ) {
+            max = l + r;
+        }
+        return Math.max(l, r) + 1;
+    }
+}
