67f533f5b944da0d412f9af407399bff53d79613
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-03-11 15:04:00 +0800

    wormchaos:解栈问题

--- src/main/java/com/wormchaos/lc/sliding_window/normal/Solution225.java
+++ src/main/java/com/wormchaos/lc/sliding_window/normal/Solution225.java
@@ -10,26 +10,38 @@ public class Solution225 {
     class MyStack {
 
         private Queue<Integer> queue;
+        private Queue<Integer> q2;
+        private int top;
 
         /** Initialize your data structure here. */
         public MyStack() {
             this.queue = new LinkedList();
+            this.q2 = new LinkedList<>();
         }
 
         /** Push element x onto stack. */
         public void push(int x) {
             queue.add(x);
+            top = x;
         }
 
         /** Removes the element on top of the stack and returns that element. */
         public int pop() {
-            queue.remove();
-            return  -1;
+            while(queue.size() > 1) {
+                int t = queue.poll();
+                q2.add(t);
+                top = t;
+            }
+            int result = queue.poll();
+            Queue temp = q2;
+            q2 = queue;
+            queue = temp;
+            return result;
         }
 
         /** Get the top element. */
         public int top() {
-            return queue.peek();
+            return top;
         }
 
         /** Returns whether the stack is empty. */
