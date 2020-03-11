723a7a8121526ca4813dafc8cf8b2bda5f1f8ea2
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-03-11 15:13:50 +0800

    wormchaos:单队列模拟栈

--- src/main/java/com/wormchaos/lc/sliding_window/normal/Solution225.java
+++ src/main/java/com/wormchaos/lc/sliding_window/normal/Solution225.java
@@ -10,41 +10,42 @@ public class Solution225 {
     class MyStack {
 
         private Queue<Integer> queue;
-        private Queue<Integer> q2;
-        private int top;
 
-        /** Initialize your data structure here. */
+        /**
+         * Initialize your data structure here.
+         */
         public MyStack() {
             this.queue = new LinkedList();
-            this.q2 = new LinkedList<>();
         }
 
-        /** Push element x onto stack. */
+        /**
+         * Push element x onto stack.
+         */
         public void push(int x) {
+            int size = queue.size();
             queue.add(x);
-            top = x;
-        }
-
-        /** Removes the element on top of the stack and returns that element. */
-        public int pop() {
-            while(queue.size() > 1) {
-                int t = queue.poll();
-                q2.add(t);
-                top = t;
+            for (int i = 0; i < size; i++) {
+                queue.add(queue.poll());
             }
-            int result = queue.poll();
-            Queue temp = q2;
-            q2 = queue;
-            queue = temp;
-            return result;
         }
 
-        /** Get the top element. */
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
+        public int pop() {
+            return queue.poll();
+        }
+
+        /**
+         * Get the top element.
+         */
         public int top() {
-            return top;
+            return queue.peek();
         }
 
-        /** Returns whether the stack is empty. */
+        /**
+         * Returns whether the stack is empty.
+         */
         public boolean empty() {
             return queue.isEmpty();
         }
