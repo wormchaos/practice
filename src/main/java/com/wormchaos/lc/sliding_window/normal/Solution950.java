dac9c5fd8b0173b4edcbb9a8fd514ea0da6d0d35
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-03-24 17:33:09 +0800

    wormchaos:优化洗牌

--- src/main/java/com/wormchaos/lc/sliding_window/normal/Solution950.java
+++ src/main/java/com/wormchaos/lc/sliding_window/normal/Solution950.java
@@ -15,17 +15,13 @@ public class Solution950 {
     public int[] deckRevealedIncreasing(int[] deck) {
         Arrays.sort(deck);
         Queue<Integer> queue = new LinkedList();
-        int[] temp = new int[deck.length];
         for (int i = 0; i < deck.length; i++) {
             queue.add(i);
         }
-        for (int i = 0; i < deck.length; i++) {
-            temp[i] = queue.poll();
-            queue.add(queue.poll());
-        }
         int[] result = new int[deck.length];
         for (int i = 0; i < deck.length; i++) {
-            result[temp[i]] = deck[i];
+            result[queue.poll()] = deck[i];
+            queue.add(queue.poll());
         }
         return result;
     }
