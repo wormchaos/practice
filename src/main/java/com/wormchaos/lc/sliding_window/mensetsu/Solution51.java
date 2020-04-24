1b8cb1f825b092aa093dc9aa718cd8435b5cf1c4
zhangwenqiang <zhangwenqiang@mobanker.com>
2020-04-24 18:32:00 +0800

    wormchaos:TODO 补充

---
+++ src/main/java/com/wormchaos/lc/sliding_window/mensetsu/Solution51.java
@@ -0,0 +1,10 @@
+package com.wormchaos.lc.sliding_window.mensetsu;
+
+/**
+ * Created by wormchaos on 2020-4-24.
+ */
+public class Solution51 {
+    public int reversePairs(int[] nums) {
+
+    }
+}
---
+++ src/main/java/com/wormchaos/lc/sliding_window/sars/Solution08_11.java
@@ -0,0 +1,31 @@
+package com.wormchaos.lc.sliding_window.sars;
+
+/**
+ * Created by wormchaos on 2020-4-23.
+ */
+public class Solution08_11 {
+    // 25 10 5 1
+    public int waysToChange(int n) {
+//        int k = 25;
+//        int[] dp = new int[n + k + 1];
+//        for (int i = 0; i < k; i++) {
+//            dp[i] = 0;
+//        }
+//        dp[k] = 1;
+//        for (int i = k + 1; i <= n + k; i++) {
+//            long t = (dp[i-1] + dp[i-5] + dp[i-10] + dp[i-25]) % 1000000007;
+//            dp[i] = (int)t;
+//        }
+//        return dp[n +k];
+//        int[] num = new int[]{25, 10, 5, 1};
+//        int[] sum = new int[4];
+//        for (int i = 0; i < num.length; i++) {
+//            sum[i] = n / num[i];
+//            n %= num[i];
+//        }
+//        int k = 0;
+//        while (k < num.length) {
+//            sum[k]
+//        }
+    }
+}
